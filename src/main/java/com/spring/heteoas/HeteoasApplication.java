2b68de0353185a719587d0a2e75f0ff5486b6193
Matheus-Freitas0 <moncaomatheus@gmail.com>
2025-07-02 17:50:05 -0300

    first commit

---
+++ src/main/java/com/spring/heteoas/HeteoasApplication.java
@@ -0,0 +1,13 @@
+package com.spring.heteoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeteoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeteoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/heteoas/HeteoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.heteoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeteoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
